9862b476434950994a68f39633e0fb1e4b996d05
John Martin <curmudgeon@rallydev.com>
2013-08-24 16:14:01 -0700

    only active states counted

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     logger: new Rally.technicalservices.logger(),
     _selected_base_records: [],
+    _direct_children: [],
     _selected_iteration: null,
     _selected_tags: [],
     items: [{
@@ -86,6 +87,9 @@ Ext.define('CustomApp', {
             itemId: 'type_selector',
             xtype: 'rallyportfolioitemtypecombobox',
             fieldLabel: 'Portfolio Item Type:',
+            storeConfig: {
+                filters: [{"property":"Ordinal","operator":">","value":0}]
+            },
             labelWidth: 135,
             listeners: {
                 change: function() {
@@ -137,25 +141,29 @@ Ext.define('CustomApp', {
         
         this.logger.log(this,this.down('#type_selector').getRecord());
         var options = {
-            pi: this.down('#type_selector').getRecord().get('ElementName'),
+            pi: this._selected_base_records[0],
             iteration: this._selected_iteration,
             tags: this._selected_tags
-            
         };
-        if ( this._selected_base_records.length > 0 ) {
-            options.pi = this._selected_base_records[0];
-        }
+
+        me._direct_children = {};
         
-        if (typeof options.pi === 'object'){
-            this._findDescendants(options, this._selected_base_records);
+        if ( options.pi.get('PortfolioItemType').Ordinal === 0 ) {
+            // we have a feature level item, so do stories instead of children
+            me.logger.log(this,"Not yet!");
         } else {
-            Ext.create('Rally.data.WsapiDataStore',{
-                model:this.down('#type_selector').getRecord().get('TypePath'),
-                fetch: ['Children','Name','FormattedID','TypePath','PortfolioItemType','Ordinal','ObjectID','Tags'],
-                autoLoad: true,
-                listeners: {
-                    load: function(store,records) {
-                        me._findDescendants(options,records);
+            options.pi.getCollection('Children',{
+                fetch:['Tags','Children','Name','FormattedID','TypePath','PortfolioItemType','Ordinal','ObjectID']
+            }).load({
+                callback: function(records,operation,success){
+                    if ( records.length === 0 ) {
+                        me.actual_chart = me.down('#actual_chart_box').add({xtype:'container',html:'The selected item has no descendants'});
+                    } else {
+                        Ext.Array.each(records,function(record){
+                            me._direct_children[record.get('FormattedID')] = record;
+                            var key = record.get('FormattedID');
+                            me._findDescendants(key,options,[record]);
+                        });
                     }
                 }
             });
@@ -211,7 +219,7 @@ Ext.define('CustomApp', {
                 {text:'State',dataIndex:'State'}
             ],
             storeConfig: {
-                fetch:['Children','Name','FormattedID','TypePath','PortfolioItemType','Ordinal','ObjectID','Tags']
+                fetch:['Children','UserStories','Name','FormattedID','TypePath','PortfolioItemType','Ordinal','ObjectID','Tags']
             },
             autoShow: true,
             height: 400,
@@ -251,16 +259,16 @@ Ext.define('CustomApp', {
             }]
          });
     },
-    _findDescendants: function(options,records){
+    _findDescendants: function(key,options,records){
         var me = this;
-        me.logger.log(this,['_findDescendants for ',records]);
+        me.logger.log(this,['_findDescendants for ',key,options,records]);
         var type_name = null;
         
         if ( records.length === 0 ) {
-            this.actual_chart = me.down('#actual_chart_box').add({xtype:'container',html:'No path to stories for the selected PI'});
+            me.logger.log(this,["No path to stories",options]);
+            me._direct_children[key].set('_leaves',[]);
         } else {
             var first_record = records[0];
-
             var pi_level = first_record.get('PortfolioItemType').Ordinal;
             options.type_name = first_record.get('PortfolioItemType').Name;
             // 0 is lowest level (e.g., feature)
@@ -281,21 +289,22 @@ Ext.define('CustomApp', {
                                 callback_counter -= 1;
                                 me.logger.log(this,'counter ' + callback_counter);
                                 if (callback_counter<=0) {
-                                    me._findDescendants(options,all_children);
+                                    me._findDescendants(key,options,all_children);
                                 }
                             }
                         });
                     }
                 });
-                if (no_children_found) { me._findDescendants(options,[]); }
+                if (no_children_found) { me._findDescendants(key,options,[]); }
             } else {
                 // get children stories                
-                me._limitPIsToSelectedTags(options,records);
+                me._limitPIsToSelectedTags(key,options,records);
             }
         }
     },
-    _getStories: function(options,records){
+    _getStories: function(key,options,records){
         var me = this;
+        me.logger.log(this,"Getting stories related to " + key);
         var oid_filters = Ext.create('Rally.data.QueryFilter',{
             property:options.type_name+".ObjectID",
             operator:'=',
@@ -312,6 +321,8 @@ Ext.define('CustomApp', {
         });
         var filters = Ext.create('Rally.data.QueryFilter',{property:"PlanEstimate",operator:'>',value:0});
         filters = filters.and(Ext.create('Rally.data.QueryFilter',{property:"DirectChildrenCount",operator:'=',value:0}));
+        filters = filters.and(Ext.create('Rally.data.QueryFilter',{property:'ScheduleState',operator:'>',value:'Defined'}));
+        
         if ( options.iteration && options.iteration.get('Name') !== "") {
             filters = filters.and(Ext.create('Rally.data.QueryFilter',{
                 property:'Iteration.Name',
@@ -329,22 +340,30 @@ Ext.define('CustomApp', {
             autoLoad: true,
             listeners: {
                 load: function(store,records) {
-                    me.logger.log(this,records);
-                    if ( records.length === 0 ) {
-                        me.actual_chart = me.down('#actual_chart_box').add({xtype:'container',html:'No stories for given selection options'});
+                    me.logger.log(this,["For " + key,records]);
+                    me._direct_children[key].set("_leaves",records);
+                    var waiter = null;
+                    for ( var i in me._direct_children ) {
+                        if ( ! me._direct_children[i].get('_leaves') ) {
+                            waiter = i;
+                        }
+                    }
+                    me.logger.log(this,me._direct_children);
+                    if ( waiter !== null ) {
+                        me.logger.log(this,"Still waiting for " + waiter);
                     } else { 
-                        me._makeChart(records);
+                        me._makeChart();
                     }
                 }
             }
         });
     },
-    _limitPIsToSelectedTags: function(options,records){
+    _limitPIsToSelectedTags: function(key,options,records){
         var me = this;
-        me.logger.log(this,['_limitPIsToSelectedTags',options]);
+        me.logger.log(this,['_limitPIsToSelectedTags',key,options,records]);
         var filtered_records = [];
         if ( options.tags.length === 0 ) {
-            me._getStories(options,records);
+            me._getStories(key,options,records);
         } else { 
             var callback_counter = 0;
             var any_tags_found = false;
@@ -356,7 +375,7 @@ Ext.define('CustomApp', {
                     callback_counter += 1;
                     record.getCollection('Tags',{fetch:['Name']}).load({
                         callback: function(tag_records,operation,success){
-                            me.logger.log(this,tag_records);
+                            me.logger.log(this,["has tags",tag_records]);
                             callback_counter -= 1;
                             me.logger.log(this,'counter ' + callback_counter);
                             Ext.Array.each(tag_records,function(tag_record) {
@@ -367,9 +386,10 @@ Ext.define('CustomApp', {
                             });
                             if (callback_counter<=0) {
                                 if ( filtered_records.length === 0 ) {
-                                    me.actual_chart = me.down('#actual_chart_box').add({xtype:'container',html:'No PIs for given selection options'});
+                                    me.logger.log(this,"Has tags, but not the right ones, so setting stories to empty");
+                                    me._direct_children[key].set("_leaves",[]);
                                 } else { 
-                                     me._getStories(options,filtered_records);
+                                     me._getStories(key,options,filtered_records);
                                 }
                             }
                         }
@@ -377,32 +397,39 @@ Ext.define('CustomApp', {
                 }
             });
             if ( !any_tags_found ) {
-                me.actual_chart = me.down('#actual_chart_box').add({xtype:'container',html:'No PIs for given selection options'});
+                me.logger.log(this,"Has no tags, so setting stories to empty");
+                me._direct_children[key].set("_leaves",[]);
             }
         }
     },
-    _makeChart: function(stories){
+    _makeChart: function(){
         var me = this;
+        me.logger.log(this,"_makeChart");
         var chart_data = {};
         var total_size = 0;
-        Ext.Array.each(stories,function(story){
-            me.logger.log(this,story.get('Name'));
-            var state = story.get('ScheduleState');
-            me.logger.log(this,state);
-            var size = story.get('PlanEstimate');
+        
+        for ( var key in me._direct_children ) {
+            var child = me._direct_children[key];
+            me.logger.log(this,[key,child]);
             
-            if ( !chart_data[state] ) {
-                chart_data[state] = 0;
-            }
-            chart_data[state] += size;
-            total_size += size;
-        });
+            var data_key = child.get('FormattedID') + ":" + child.get('Name');
+            chart_data[data_key] = 0;
+            
+            var leaves = child.get('_leaves');
+            me.logger.log(this,leaves);
+            
+            Ext.Array.each(leaves,function(leaf){
+                var size = leaf.get('PlanEstimate') || 0;
+                chart_data[data_key] += size;
+                total_size += size;
+            });
+        };
 
         var series = [];
-        for ( var state in chart_data ) {
-            var ratio = parseInt(100*chart_data[state]/total_size);
-            var name = state + " " + ratio + "%";
-            series.push({name:name,y:chart_data[state]});
+        for ( var key in chart_data ) {
+            var ratio = parseInt(100*chart_data[key]/total_size);
+            var name = Ext.util.Format.ellipsis(key,40,true) + " " + ratio + "%";
+            series.push({name:name,y:chart_data[key]});
         }
         
         me.logger.log(this,["Chart Data",chart_data]);
@@ -410,34 +437,40 @@ Ext.define('CustomApp', {
 
         if ( me.actual_chart ) { me.actual_chart.destroy(); }
         
-        me.actual_chart = this.down('#actual_chart_box').add({
-            xtype:'rallychart',
-
-            chartConfig: {
-                chart: {},
-                height: 350,
-                width: 350,
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            color: '#000000',
-                            connectorColor: '#000000',
-                            format: '<b>{point.name}</b>'
+        if ( total_size === 0 ) {
+            me.actual_chart = this.down('#actual_chart_box').add({
+                xtype:'container',
+                html:'No points found'
+            });
+        } else {
+            me.actual_chart = this.down('#actual_chart_box').add({
+                xtype:'rallychart',
+                chartConfig: {
+                    chart: {},
+                    height: 350,
+                    width: 350,
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                color: '#000000',
+                                connectorColor: '#000000',
+                                format: '<b>{point.name}</b>'
+                            }
                         }
+                    },
+                    tooltip: { enabled: false},
+                    title: {
+                        text: 'Actual Distribution',
+                        align: 'center'
                     }
                 },
-                tooltip: { enabled: false},
-                title: {
-                    text: 'Actual Distribution',
-                    align: 'center'
+                chartData: {
+                    series: [{type:'pie',name:'State Distribution',data:series}]
                 }
-            },
-            chartData: {
-                series: [{type:'pie',name:'State Distribution',data:series}]
-            }
-        });
+            });
+        }
     }
 });
